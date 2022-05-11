6dc3f85d9923337e77824ec7dc84bb37078023e2
Tony <Tony@chaoWei.com>
2022-05-11 16:57:45 +0800

    变更

---
+++ src/Advanced/Hero.java
@@ -0,0 +1,16 @@
+package Advanced;
+/**
+* @ author: Tony
+* @ data:  2022/5/11 16:46
+* @ version: 1.0
+* @ description:TODO
+*/
+public class Hero {
+    String name = "默认值";
+    public void test(){
+        String.name = "盖伦";
+        System.out.println(name);
+    }
+
+
+}
