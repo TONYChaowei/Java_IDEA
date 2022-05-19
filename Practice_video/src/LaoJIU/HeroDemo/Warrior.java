b91a7826e3bd132cb41f2074ac938bf4ef4f9df3
Tony <Tony@chaoWei.com>
2022-05-19 14:25:41 +0800

    RGB

--- Practice_video/src/LaoJIU/HeroDemo/Warrior.java
+++ Practice_video/src/LaoJIU/HeroDemo/Warrior.java
@@ -10,13 +10,28 @@ import LaoJIU.HeroDemo.Hero;
  */
 public class Warrior extends Hero {
     private int pysicalAttach;     //战士特有属性，物理攻击力
-    public Warrior() {}
+    public Warrior() {
+        setNickname("默认战士");
+        setLevel(1);
+        setMaxLife(1500);
+    }
 
     public Warrior(String nickname,int pysicalAttach) {
         setNickname(nickname);
         setPysicalAttach(pysicalAttach);
     }
-
+    public String toString() {
+        StringBuffer str = new StringBuffer();
+        str.append(getLevel());
+        str.append("\t");
+        str.append(getNickname());
+        str.append("\t");
+        str.append(getMaxLife());
+        str.append("\t");
+        str.append(getCurrLife());
+        str.append("\t");
+        return str.toString();
+    }
     @Override
     //重写Object类的equals方法，以便比较两个对象是否相等
     public boolean equals(Object obj) {
