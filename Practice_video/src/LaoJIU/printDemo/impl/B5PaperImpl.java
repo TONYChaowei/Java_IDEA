232dc694ebf57faf10353964bce65dab5751a411
Tony <Tony@chaoWei.com>
2022-06-13 16:17:48 +0800

    commit

---
+++ Practice_video/src/LaoJIU/printDemo/impl/B5PaperImpl.java
@@ -0,0 +1,16 @@
+package LaoJIU.printDemo.impl;
+
+import LaoJIU.printDemo.Iface.Paper;
+
+/**
+ * @ author:Tony
+ * @ date 2022/6/13 16:15
+ * @ version 1.0
+ * @ description:TODO
+ */
+public class B5PaperImpl implements Paper {
+    @Override
+    public String getSize() {
+        return "B5ж╫уе";
+    }
+}
