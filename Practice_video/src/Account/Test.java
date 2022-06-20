8045b9f68bbf4d6e6deaec863b6bddf3f59c1e29
Tony <Tony@chaoWei.com>
2022-06-20 12:19:48 +0800

    commti

---
+++ Practice_video/src/Account/Test.java
@@ -0,0 +1,20 @@
+package Account;
+
+
+import java.util.Scanner;
+
+/**
+ * @ author:Tony
+ * @ date 2022/6/19 19:44
+ * @ version 1.0
+ * @ description:TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("«Î ‰»Îµ•º€");
+        Scanner input = new Scanner(System.in);
+        double price = input.nextDouble();
+        
+
+    }
+}
---
+++ Practice_video/src/LaoJIU/UnCheckedException.java
@@ -0,0 +1,20 @@
+package LaoJIU;
+
+import sun.applet.Main;
+
+/**免检查异常
+ * @ author:Tony
+ * @ date 2022/6/15 21:01
+ * @ version 1.0
+ * @ description:TODO
+ */
+public class UnCheckedException {
+    public static void main(String[] args) {
+//        String value = null;    //遇到空指针异常5
+//        System.out.println(value.toString());
+        //将字符串转换换为数字
+        String str = "abc";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
+    }
+}
