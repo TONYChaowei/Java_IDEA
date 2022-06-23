e9bd6ee008ac3b44d59d160b3e72be12c467b43c
Tony <Tony@chaoWei.com>
2022-06-23 22:26:49 +0800

    commti

---
+++ Practice_video/src/LaoJIU/LessThanZeroException.java
@@ -0,0 +1,17 @@
+package LaoJIU;
+
+/** 自定义异常 - 小于0时触发
+ * @ author:Tony
+ * @ date 2022/6/22 12:50
+ * @ version 1.0
+ * @ description:TODO
+ */
+public class LessThanZeroException extends Exception {
+    private int value;
+    public lessThanZeroException(){
+        super("数字不能小于零！");
+    }
+    public LessThanZeroException(int value){
+        super("输入的数字为" + value + "不能小于零！");
+    }
+}
--- Practice_video/src/LaoJIU/ThrowDemo.java
+++ Practice_video/src/LaoJIU/ThrowDemo.java
@@ -1,10 +1,63 @@
 package LaoJIU;
 
-/**
+/**英雄异常经验值
  * @ author:Tony
- * @ date $ $
+ * @ date 2022/6/22 11:45
  * @ version 1.0
  * @ description:TODO
  */
 public class ThrowDemo {
+    public static void main(String[] args) {
+            Hero hero = new Hero("貂蝉");
+        try {
+            hero.setExp(-1000);
+        } catch (Exception e) {
+            //throw new RuntimeException(e);
+           //   e.printStackTrace();
+            System.err.print("在设置经验值时出现异常：" + e.getMessage());
+        }
+        System.out.println("当前英雄的经验值" + hero.getExp());
+    }
+
+
+}
+class Hero{
+    private String name;
+    private long exp;
+    private int health;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    Hero(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getExp() {
+        return exp;
+    }
+
+    public void setExp(long exp) throws Exception {
+        if (exp >= 0){
+            this.exp = exp;
+        }else{
+            throw new Exception("经验值不能为负数");
+        }
+    }
+    public static rideHorse(){
+        System.out.println(name + "正在快乐的骑马！");
+    }
+
 }
